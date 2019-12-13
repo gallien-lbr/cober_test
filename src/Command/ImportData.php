59acf46d8e02b0c2fe0575115c797190d2df8b91
Gallien L <1328920+glb2154@users.noreply.github.com>
2019-12-13 11:56:07 +0100

    feat: cli import-data => add capability to unzip + insert data in DB

--- src/Command/ImportData.php
+++ src/Command/ImportData.php
@@ -2,14 +2,26 @@
 
 namespace App\Command;
 
+use App\Entity\Company;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\HttpFoundation\File\Exception\FileNotFoundException;
+use League\Csv\Reader;
+
 
 class ImportData extends Command
 {
     // the name of the command (the part after "bin/console")
     protected static $defaultName = 'app:import-data';
+    private $em;
+
+    public function __construct(string $name = null,EntityManagerInterface $em)
+    {
+        parent::__construct($name);
+        $this->em = $em;
+    }
 
     protected function configure()
     {
@@ -25,22 +37,58 @@ class ImportData extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $f = 'http://files.data.gouv.fr/sirene/sirene_2018088_E_Q.zip';
-        //$f = 'http://httpbin.org';
+        $url = 'http://files.data.gouv.fr/sirene/';
+        $date = "2018088";
+        $fname = 'sirene_'.$date.'_E_Q.zip';
+        $input = $url . $fname;
+
         $client = new \GuzzleHttp\Client();
 
         try{
-            $request = new \GuzzleHttp\Psr7\Request('GET', $f) ;
-            $promise = $client->sendAsync($request)->then(function ($response) {
-                echo 'I completed! ' ;//. $response->getBody();
+            $request = new \GuzzleHttp\Psr7\Request('GET', $input) ;
+            echo "\n Try to download $input";
+            $promise = $client->sendAsync($request)->then(function ($response) use ($fname) {
+
+                $dir =  __DIR__ .'/../../downloads' ;
+
+                if (!file_exists($dir)){
+                   throw new FileNotFoundException($dir);
+                }
+                $dest = $dir .'/' .$fname;
+                file_put_contents  ($dest,$response->getBody());
+
+                $zip = new \ZipArchive;
+                if ($zip->open($dest)) {
+                    $zip->extractTo($dir);
+
+                    // assuming only one file per zip
+                    $filename = $zip->getNameIndex(0);
+                    $zip->close();
+
+                    echo "\n Successfuly unzipped : " . $fname  . " to: " . $filename;
+                    echo "\n Removing ZIP " . $fname;
+
+                } else {
+                    echo "\n Failed to unzip " . $fname;
+                }
+
             });
 
             $promise->wait();
 
-        }catch(\Throwable $t){
+            $company = new Company();
+            $company->setSiren(123456789);
+            $company->setL1Declaree('test');
+
+            $this->em->persist($company);
+            $this->em->flush();
+
+        } catch(\Throwable $t){
             echo "Something wrong happened";
+            echo $t->getMessage();
         }
 
         return 0;
     }
+
 }
